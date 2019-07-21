60b52f4cf0ee9cb331abaac7467df39f26bbc615
Syoyo Fujita <syoyo@lighttransport.com>
2019-07-21 16:02:22 +0900

    Update cross-compilation script.

--- scripts/bootstrap-aarch64-linux-cross.sh
+++ scripts/bootstrap-aarch64-linux-cross.sh
@@ -3,10 +3,11 @@
 rm -rf build-aarch64-cross
 mkdir build-aarch64-cross
 
-# Set path to aarch64 build of zlib 
-PTEX_ZLIB_PATH=$HOME/local/aarch64-cross
+# Set path to aarch64 build of zlib
+PTEX_ZLIB_PATH=$HOME/local/aarch64/
 
 cmake -Bbuild-aarch64-cross -H. \
-  -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc \
-  -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ \
+  -DCMAKE_INSTALL_PREFIX=$HOME/local/aarch64/ \
+  -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-5 \
+  -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++-5 \
   -DPTEX_ZLIB_PATH=${PTEX_ZLIB_PATH}
